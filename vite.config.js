85343eeda532c8b0a1607581c7caa90e8bc2fd41
Duarte Almeida <duartemalmeida@gmail.com>
2023-07-14 13:36:48 +0100

    feat(app): fix site headings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/styles/core/mixin.scss";'
+      }
+    }
+  },
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve('./src'),

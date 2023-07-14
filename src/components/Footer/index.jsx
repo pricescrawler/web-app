374d5f97913be644a487a245a1443b5fcabbe6ba
Duarte Almeida <72300662+DMCFA@users.noreply.github.com>
2023-07-14 10:34:53 +0100

    feat(footer): add mobile link (#66)
    * feat(footer): add mobile link
    
    * feat(app): add conditional render of mobile url
    
    ---------
    
    Co-authored-by: Sandro Ferreira <sandroacferreira@gmail.com>

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@
  */
 
 import './index.scss';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 /**
@@ -13,12 +13,13 @@ import { useTranslation } from 'react-i18next';
 
 function Footer() {
   const { t } = useTranslation();
+  const [mobileAppUrl] = useState(import.meta.env.VITE_MOBILE_APP_URL);
 
   return (
     <div className={'nav-footer-container'}>
       <div className={'nav-footer'}>
         <Link
-          className={'nav-footer-link me-3'}
+          className={'nav-footer-link'}
           to={'/about'}
         >
           {t('menu.about')}
@@ -29,6 +30,16 @@ function Footer() {
         >
           {t('menu.privacy-terms')}
         </Link>
+        {mobileAppUrl && (
+          <Link
+            className={'nav-footer-link'}
+            rel={'noopener noreferrer'}
+            target={'_blank'}
+            to={mobileAppUrl}
+          >
+            {t('menu.mobile-app')}
+          </Link>
+        )}
       </div>
     </div>
   );

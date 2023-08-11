3587cc581f5a6469cb51ba84be1351fe1ec76e14
Sandro Ferreira <sandroacferreira@gmail.com>
2023-08-11 17:56:26 +0100

    feat(app): add google charts and update footer (#72)
    * feat(app): add google charts and update footer
    
    * feat(app): minor improvements

--- src/services/utils/index.js
+++ src/services/utils/index.js
@@ -27,7 +27,7 @@ export const getFormattedPrice = (product) => {
 };
 
 /**
- *  Gets the last price of a product from its list of prices.
+ * Gets the last price of a product from its list of prices.
  * @param {object} product - The product object.
  * @returns {number} The last price of the product as a float number.
  * */
@@ -38,6 +38,20 @@ export const getLastPrice = (product) => {
   }
 
   return 0;
+};
+
+/**
+ * Get Average Price.
+ * @param {Array} prices - Array of prices.
+ * @returns {string} - Average price rounded to two decimal places.
+ * */
+
+export const getAveragePrice = (prices) => {
+  const sum = prices
+    .map((price) => parseFloat(getFormattedPrice(price)))
+    .reduce((acc, val) => acc + val, 0);
+
+  return (sum / prices.length).toFixed(2);
 };
 
 /**
